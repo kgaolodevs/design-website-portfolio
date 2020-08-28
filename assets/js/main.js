bc5f8c3d84573bd3ef5f37a7179c5662818a2ab7
kgaolodevs <kgaolodev@gmail.com>
2020-08-28 16:24:47 +0200

    Completed website

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,3 +64,17 @@ sr.reveal(".about__subtitle", { delay: 300 });
 sr.reveal(".about__profession", { delay: 400 });
 sr.reveal(".about__text", { delay: 500 });
 sr.reveal(".about__social-icon", { delay: 600, interval: 200 });
+
+/* ////////// SCROLL SKILLS ////////// */
+sr.reveal(".skills__subtitle", { delay: 500 });
+sr.reveal(".skills__name", { distance: "20px", delay: 50, interval: 100 });
+sr.reveal(".skills__img", { delay: 400 });
+
+/* ////////// SCROLL PORTFOLIO ////////// */
+sr.reveal(".portfolio__img", { interval: 200 });
+
+/* ////////// SCROLL CONTACT ////////// */
+sr.reveal(".contact__subtitle", {});
+sr.reveal(".contact__text", { delay: 200 });
+sr.reveal(".contact__input", { delay: 400 });
+sr.reveal(".contact__button", { delay: 600 });
